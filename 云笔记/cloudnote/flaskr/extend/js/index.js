ee5eb02bbf03111d162f38d01c4ee7512fe93628
fanaiai <516039800@qq.com>
2018-08-27 18:30:02 +0800

    1

--- 云笔记/cloudnote/flaskr/extend/js/index.js
+++ 云笔记/cloudnote/flaskr/extend/js/index.js
@@ -9,4 +9,12 @@ $("body").append(dom);
 
 var cssselector=new CssSelector();
 var highlight=new HightLight();
+$("body").append("<div id='caiyun-root' class='caiyun-root caiyun-highlight' style='width:0;height:0'></div>");
+    var rootshadow = $("#caiyun-root")[0].createShadowRoot();
+    
+$(document).mouseover(function(e){
+	var ele=e.target;
+	var sizes = hightlight.getSizes(ele);
+    hightlight.addHoverShadowDom(rootshadow, sizes);
+})
 
